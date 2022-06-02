0db4f61d87aa340db9ff9fc2b1475c09a9ac3b24
silent10z <dkgueing@gmail.com>
2022-06-03 00:56:21 +0900

    reatactor: 모달창 수정

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -27,22 +27,71 @@ const Modal = ({ setIsOpenPopup, info }: Props) => {
         <header>
           <div>자세히 보기</div>
           <div>도움이 필요해요!</div>
-          <div>close</div>
+          <button type='button' onClick={handleCloseButtonClick}>
+            close
+          </button>
         </header>
         <div className={styles.imgContainer}>
           <img src={info.popfile} alt='유기동물 사진' />
         </div>
         <div className={styles.textContainer}>
-          <div>
+          <div className={styles.rightWapper}>
             <dl>
-              <dt>나이</dt>
-              <dd>{info.age}</dd>
+              <div>
+                <dt>나이:</dt>
+                <dd>{info.age}</dd>
+              </div>
+              <div>
+                <dt>성별:</dt>
+                <dd>{info.sexCd === 'M' ? '수컷' : '암컷'}</dd>
+              </div>
+              <div>
+                <dt>상태:</dt>
+                <dd>{info.processState}</dd>
+              </div>
+              <div>
+                <dt>몸무게:</dt>
+                <dd>{info.weight}</dd>
+              </div>
+              <div>
+                <dt>종:</dt>
+                <dd>{info.kindCd}</dd>
+              </div>
+              <div>
+                <dt>특징:</dt>
+                <dd>{info.specialMark}</dd>
+              </div>
             </dl>
           </div>
-          <div>
-            <dt>나이</dt>
-            <dd>{info.age}</dd>
+          <div className={styles.leftWapper}>
+            <div>
+              <dt>발견일:</dt>
+              <dd>{info.happenDt}</dd>
+            </div>
+            <div>
+              <dt>구조위치:</dt>
+              <dd>{info.noticeNo}</dd>
+            </div>
+            <div>
+              <dt>보호소:</dt>
+              <dd>{info.careNm}</dd>
+            </div>
+            <div>
+              <dt>보호소 번호:</dt>
+              <dd>{info.careTel}</dd>
+            </div>
+            <div>
+              <dt>보호소 주소:</dt>
+              <dd>{info.careAddr}</dd>
+            </div>
+            <div>
+              <dt>담당 보호사:</dt>
+              <dd>{info.chargeNm}</dd>
+            </div>
           </div>
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button size='large'>친구 목록에 담기 </Button>
         </div>
       </div>
     </div>
