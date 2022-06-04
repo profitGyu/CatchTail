eed4c62496926279d8d0aba783bb275d5fc6fe9c
silent10z <dkgueing@gmail.com>
2022-06-05 03:14:26 +0900

    reafactor: 메인 글자 리펙토링

--- src/routes/home/chart/index.tsx
+++ src/routes/home/chart/index.tsx
@@ -33,24 +33,23 @@ const BarChart = () => {
   const tooltip = CustomTooltip()
   const TICK_STYLE = {
     axis: { stroke: 'transparent' },
-    tickLabels: { fontSize: 16, fontFamily: 'Roboto', fill: '#2f4858' },
-    ticks: { stroke: 'none' },
-    grid: { stroke: 'none' },
+    tickLabels: { fontSize: 16, fontFamily: 'Roboto', fill: '#2f4858', padding: 10 },
   }
 
   return (
     <div className={styles.barChart}>
-      <h1>최근 5년간 국내 유기동물 발생 현황</h1>
-      <VictoryChart domainPadding={{ x: [80, 0], y: [100, 10] }} width={600} height={400}>
-        <VictoryAxis
-          tickValues={[2022, 2021, 2020, 2019, 2018]}
-          // tickLabelComponent={<VictoryLabel dx={40} />}
-          style={TICK_STYLE}
+      <VictoryChart domainPadding={{ x: [100, 0], y: [30, 0] }} width={600} height={400}>
+        <VictoryLabel
+          x={25}
+          y={15}
+          style={{ fontFamily: 'inherit', fontSize: '28px', fontWeight: 'bold', fill: '#2f4858' }}
+          text='최근 5년간 국내 유기동물 발생 현황'
         />
+        <VictoryAxis tickValues={[2022, 2021, 2020, 2019, 2018]} style={TICK_STYLE} />
         <VictoryAxis
           dependentAxis
           tickLabelComponent={<VictoryLabel dx={60} />}
-          tickFormat={(x) => `${x.toLocaleString()}마리`}
+          tickFormat={(x) => `${x.toLocaleString()}`}
           style={TICK_STYLE}
         />
         <VictoryLine
@@ -58,15 +57,18 @@ const BarChart = () => {
           x='yaer'
           y='count'
           labels={({ datum }) => datum.count.toLocaleString()}
+          animate={{ duration: 1000, easing: 'bounce' }}
           style={{
             data: { stroke: 'transparent' },
             parent: { border: '1px solid #ccc' },
+            labels: { fill: '#2f4858' },
           }}
         />
         <VictoryStack colorScale={['#f4dc9c', '#b0c48d', '#9e9e9e']}>
           {DATALIST.map(({ id, data }) => {
             return (
               <VictoryBar
+                animate={{ duration: 1000, easing: 'bounce' }}
                 key={`${id}-${data}`}
                 data={data}
                 x='yaer'
@@ -92,8 +94,13 @@ const BarChart = () => {
           style={{ title: { fontSize: 20 }, labels: { fill: '#2f4858', fontWeight: 700 } }}
           data={[{ name: '개' }, { name: '고양이' }, { name: '기타' }]}
         />
+        <VictoryLabel
+          x={200}
+          y={450}
+          style={{ fontFamily: 'inherit', fontSize: '12px', fill: '#a0a0a0' }}
+          text='*2022년 6월4일까지의 자료입니다.'
+        />
       </VictoryChart>
-      <p>*2022년 6월4일까지의 자료입니다.</p>
     </div>
   )
 }
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -6,26 +6,30 @@ import BarChart from './chart'
 
 const Home = () => {
   return (
-    <div className={styles.home}>
-      <section className={styles.intoSection}>
-        <h1>사지않고 입양하는 문화를 만들기 위해 노력합니다.</h1>
-        <br />
-        <h2> Catch Tail은</h2>
-        <h2>
-          <TypeIt
-            options={{
-              speed: 50,
-            }}
-          >
-            잊혀져가는 유기동물들을 세상에 알리고 따뜻한 손길을 전해주는 서비스 입니다.
-          </TypeIt>
-        </h2>
-        <h4>유기동물 입양,실종 정보를 조회할 수 있습니다.</h4>
-      </section>
+    <div className={styles.main}>
+      <div className={styles.home}>
+        <section className={styles.intoSection}>
+          <h1>사지않고 입양하는 문화를 만들기 위해 노력합니다.</h1>
+          <br />
+          <h2> Catch Tail은</h2>
+          <h2>
+            <TypeIt
+              options={{
+                speed: 50,
+              }}
+            >
+              잊혀져가는 유기동물들을 세상에 알리고 따뜻한 손길을 전해주는 서비스 입니다.
+            </TypeIt>
+          </h2>
+          <h4>유기동물 입양,실종 정보를 조회할 수 있습니다.</h4>
+        </section>
+      </div>
       <section className={styles.chartSection}>
-        <h2>매년 전국적으로 10만 마리 이상의 유기동물들이 보호소로 구조되고 있습니다.</h2>
-        <h4>안탑깝게도 이중 절반에 가까운 동물들은 다시 가족을 만나지 못합니다.</h4>
         <BarChart />
+        <div className={styles.textSection}>
+          <h2>매년 전국적으로 10만 마리 이상의 유기동물들이 보호소로 구조되고 있습니다.</h2>
+          <h4>안탑깝게도 이중 절반에 가까운 동물들은 다시 가족을 만나지 못합니다.</h4>
+        </div>
       </section>
       <section>안녕</section>
     </div>
