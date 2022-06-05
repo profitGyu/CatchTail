cc834fdec0c2e50ee29317fca88eaa5f9adbb3e1
silent10z <dkgueing@gmail.com>
2022-06-05 14:30:18 +0900

    feat 검색 결과가 없는 경우 추가

--- src/routes/findAnimal/searchResult/index.tsx
+++ src/routes/findAnimal/searchResult/index.tsx
@@ -6,6 +6,7 @@ import Skeleton from '../skeleton'
 import ItemBox from 'components/ItemBox'
 import { useInView } from 'react-intersection-observer'
 import { useEffect } from 'react'
+import { isEmpty } from 'lodash'
 
 const SearchResult = () => {
   const [ref, inView] = useInView()
@@ -26,11 +27,12 @@ const SearchResult = () => {
               // eslint-disable-next-line react/no-array-index-key
               return <Skeleton key={`skeleton-${i}`} />
             })
-          : data?.pages.map((page) =>
-              page.items.item.map((item) => {
+          : data?.pages.map((page) => {
+              if (isEmpty(page.items.item)) return <div>검색 결과가 없습니다.</div>
+              return page.items.item.map((item) => {
                 return <ItemBox item={item} key={item.desertionNo} />
               })
-            )}
+            })}
         {!isLoading && <div style={{ width: '100%', height: '50px' }} ref={ref} />}
       </ul>
     </div>
